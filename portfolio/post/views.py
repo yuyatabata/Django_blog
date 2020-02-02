ef0218f86184aad03f03244de23dacb57919c6ca
田端佑也 <tabayu5@gmail.com>
2020-02-03 08:39:26 +0900

    add templates

--- portfolio/post/views.py
+++ portfolio/post/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world")
-# Create your views here.
+    return render(request, 'post/index.html')
