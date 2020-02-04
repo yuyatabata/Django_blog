24041dc8c35af2c8278e39e6265fc8ea79d7042c
田端佑也 <tabayu5@gmail.com>
2020-02-04 09:25:42 +0900

    post

--- portfolio/post/models.py
+++ portfolio/post/models.py
@@ -5,3 +5,9 @@ class Post(models.Model):
     published = models.DateTimeField()
     image = models.ImageField(upload_to='media/')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
--- portfolio/post/views.py
+++ portfolio/post/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-    return render(request, 'post/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'post/index.html',{'posts': posts})
+
